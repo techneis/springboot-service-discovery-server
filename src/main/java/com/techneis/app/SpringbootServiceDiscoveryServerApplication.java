73e66e655d76bbada27797c3ded771441c16e0fc
Neeraj <techneis@gmail.com>
2022-12-23 19:27:11 +0530

    initial commit

---
+++ src/main/java/com/techneis/app/SpringbootServiceDiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package com.techneis.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringbootServiceDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiceDiscoveryServerApplication.class, args);
+	}
+
+}
